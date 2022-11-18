23309936022076372955820834f805f428454b9f
ramuunnava <ramuunnava1@gmail.com>
2022-11-18 00:54:34 -0600

    sds

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username }, function (err, user) {
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  });
+  }))
 var mongoose = require('mongoose');
 var Car = require("./models/car");
 const bodyParser = require("body-parser"); 
@@ -45,6 +60,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+ }));
+ app.use(passport.initialize());
+ app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
--- routes/car.js
+++ routes/car.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var router = express.Router();
-const bodyParser = require("body-parser"); 
-router.use(bodyParser.json());
 
+const car_controlers= require('../controllers/car');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('car', { title: 'Search result for Car' });
 });
 
-const car_controlers= require('../controllers/car');
+var router = express.Router();
 router.get('/',car_controlers.car_view_all_Page );
 router.get('/detail',car_controlers.car_view_one_Page);
 router.get('/create', car_controlers.car_create_Page);
